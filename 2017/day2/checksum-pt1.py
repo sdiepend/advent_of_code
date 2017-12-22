ff81f495c76dedaa6bd737b9cf816ef30c0f3687
Stijn Diependaele <stijn.diependaele@vdab.be>
2017-12-22 20:42:50 +0100

    change folder names

--- 2017/day2/checksum-pt1.py
+++
@@ -1,9 +0,0 @@
-with open("input") as f:
-    numbers = [list(map(int,x.strip().split('\t'))) for x in f.readlines()]
-
-sum = 0
-
-for line in numbers:
-    sum = sum + max(line) - min(line)
-
-print(sum)
